b85da1d342d63856080e4283a97f4b1174a2cdf8
Rohan Gupta <rohangupta569@gmail.com>
2021-02-25 23:16:49 +0530

    add a router for users with controller for users profile and post

---
+++ controllers/user_controller.js
@@ -0,0 +1,6 @@
+module.exports.profile = function(req,res){
+    return res.end('<h1>Users Profile !!</h1>');
+}
+module.exports.post = function(req,res){
+    return res.end('<h1>Users Post !!</h1>');
+}
