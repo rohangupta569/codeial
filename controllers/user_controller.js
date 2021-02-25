3aa20107b5d77518d4a59e110c1fbff21a7c8a11
Rohan Gupta <rohangupta569@gmail.com>
2021-02-25 23:53:52 +0530

    add view to home and user

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,6 +1,16 @@
+// module.exports.profile = function(req,res){
+//     return res.end('<h1>Users Profile !!</h1>');
+// }
+// module.exports.post = function(req,res){
+//     return res.end('<h1>Users Post !!</h1>');
+// }
 module.exports.profile = function(req,res){
-    return res.end('<h1>Users Profile !!</h1>');
+    return res.render('users',{
+        title : "Users profile"
+    });
 }
 module.exports.post = function(req,res){
-    return res.end('<h1>Users Post !!</h1>');
+    return res.render('users',{
+        title : "Users Post"
+    });
 }
