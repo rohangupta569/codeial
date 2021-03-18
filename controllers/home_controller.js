3475ccbbf7820df40ffe61401391e888c97c1a1a
Rohan Gupta <rohangupta569@gmail.com>
2021-03-19 01:44:27 +0530

    creating schema for posts

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -2,9 +2,18 @@
 //     return res.end('<h1>Express is up for Codeial!</h1>');
 // }
 module.exports.home = function(req,res){
-    console.log(req.cookies);
+   /* console.log(req.cookies);
     res.cookie('user_id',25);
     return res.render('home',{
         title : 'Home'
+    });*/
+    if(req.isAuthenticated()){
+        return res.render('home',{
+            title : 'Home'
+        });
+    }
+
+    return res.render('user_sign_in',{
+        title: "Codeial | Sign In"
     });
 }
