248aca471e07cb8432163cbf22fbfe6535e107dd
Rohan Gupta <rohangupta569@gmail.com>
2021-03-20 01:17:42 +0530

    Display Posts and related user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,12 +1,24 @@
 // module.exports.home = function(req,res){
 //     return res.end('<h1>Express is up for Codeial!</h1>');
 // }
+const Post = require('../models/post');
 module.exports.home = function(req,res){
-    console.log(req.cookies);
-    res.cookie('user_id',25);
-    return res.render('home',{
-        title : 'Home'
+    // console.log(req.cookies);
+    // res.cookie('user_id',25);
+
+    //here we can only access the user id stored in our posts user , but by populating user we can access whole user object
+    // Post.find({},function(err,posts){
+    //     return res.render('home',{
+    //         title : 'Codeial | Home',
+    //         posts: posts
+    // });
+    //Populate the user of each post 
+    Post.find({}).populate('user').exec(function(err,posts){
+        return res.render('home',{
+            title : 'Codeial | Home',
+            posts: posts
     });
+  });
     // if(req.isAuthenticated()){
     //     return res.render('home',{
     //         title : 'Home'
