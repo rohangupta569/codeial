bd20280f0dfda2bbd26cacaef6ee9205a6421495
Rohan Gupta <rohangupta569@gmail.com>
2021-03-25 00:26:11 +0530

    Deleting a comment (Authorized)

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -20,3 +20,20 @@ module.exports.create = function(req,res){
         }
     });
 }
+
+module.exports.destroy = function(req,res){
+    Comment.findById(req.params.id, function(err,comment){
+        if(comment.user == req.user.id){
+
+            let postId = comment.post;
+
+            Comment.remove();
+
+            Post.findByIdAndUpdate(postId,{$pull:{comments:req.params.id}},function(err,comment){
+                return res.redirect('back');
+            });
+        }else{
+            return res.redirect('back');
+        }
+    });
+}
--- routes/comments.js
+++ routes/comments.js
@@ -5,5 +5,6 @@ const Passport = require('passport');
 const commentController = require('../controllers/comments_controller');
 
 router.post('/create',Passport.checkAuthentication,commentController.create);
+router.get('/destroy/:id',Passport.checkAuthentication,commentController.destroy);
 
 module.exports = router;
