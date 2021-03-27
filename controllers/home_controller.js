c4d71d616f1899f6716b3a6e5b0460f849e1bee4
Rohan Gupta <rohangupta569@gmail.com>
2021-03-28 01:22:11 +0530

    Converting code to async await

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -4,43 +4,79 @@
 const Post = require('../models/post');
 const User = require('../models/user');
 
-module.exports.home = function(req,res){
-    // console.log(req.cookies);
-    // res.cookie('user_id',25);
+//using async await 
+module.exports.home = async function(req,res){
 
-    //here we can only access the user id stored in our posts user , but by populating user we can access whole user object
-    // Post.find({},function(err,posts){
-    //     return res.render('home',{
-    //         title : 'Codeial | Home',
-    //         posts: posts
-    // });
-    //Populate the user of each post 
-    Post.find({})
+  try {
+    //populate the user of each post
+    let posts = await Post.find({})
     .populate('user')
     .populate({
-      path : 'comments',
-      populate: {
-        path :'user'
-      }
-    })
-    .exec(function(err,posts){
-
-      User.find({},function(err,users){
-        return res.render('home',{
-          title : 'Codeial | Home',
-          posts: posts,
-          all_users: users
-      });
-        
+    path : 'comments',
+    populate: {
+      path :'user'
+   }
+   });
+ 
+   let users = await User.find({});
+ 
+   return res.render('home',{
+    title : 'Codeial | Home',
+    posts: posts,
+    all_users: users
     });
-  });
-    // if(req.isAuthenticated()){
-    //     return res.render('home',{
-    //         title : 'Home'
-    //     });
-    // }
-
-    // return res.render('user_sign_in',{
-    //     title: "Codeial | Sign In"
-    // });
+  } catch (err) {
+    console.log('Error',err);
+  }
 }
+
+
+//before we were using async await
+// module.exports.home = function(req,res){
+//     // console.log(req.cookies);
+//     // res.cookie('user_id',25);
+
+//     //here we can only access the user id stored in our posts user , but by populating user we can access whole user object
+//     // Post.find({},function(err,posts){
+//     //     return res.render('home',{
+//     //         title : 'Codeial | Home',
+//     //         posts: posts
+//     // });
+//     //Populate the user of each post 
+//     Post.find({})
+//     .populate('user')
+//     .populate({
+//       path : 'comments',
+//       populate: {
+//         path :'user'
+//       }
+//     })
+//     .exec(function(err,posts){
+
+//       User.find({},function(err,users){
+//         return res.render('home',{
+//           title : 'Codeial | Home',
+//           posts: posts,
+//           all_users: users
+//       });
+        
+//     });
+//   });
+//     // if(req.isAuthenticated()){
+//     //     return res.render('home',{
+//     //         title : 'Home'
+//     //     });
+//     // }
+
+//     // return res.render('user_sign_in',{
+//     //     title: "Codeial | Sign In"
+//     // });
+// }
+
+
+//using then 
+//Post.find({}).populate('comments').then(function());
+
+//using promise
+// let posts = Post.find({}).populate('comments').exec();
+//posts.then();
