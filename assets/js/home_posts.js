9a796275dedc98e86c88edbb4c1c4dabd7b75a2f
Rohan Gupta <rohangupta569@gmail.com>
2021-04-01 02:33:41 +0530

    Creating a post :: receiving data (ajax)

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -11,7 +11,8 @@
                 url : '/posts/create',
                 data : newPostForm.serialize(),
                 success : function(data){
-                    console.log(data);
+                    let newPost = newPostDom(data.data.post);
+                    $('#posts-list-container>ul').prepend(newPost);
                 }, error : function(error){
                     console.log(error.responseText);
                 }
@@ -19,7 +20,35 @@
         });
     }
 
-    createPost();
+    //method to create post in DOM
+    let newPostDom = function(post){
+        return $(`<li id="post-${post._id}">
+        <p>
+            <small>
+                <a class="delete-post-button" href="/posts/destroy/${post.id}">X</a>
+            </small>
+        ${post.content}
+        <br>
+        <small>
+            ${post.user.name}
+        </small>
+        </p>
+        <div class="post-comments">
 
-    //method to create a post in DOM
+                <form action="/comments/create" method ="POST">
+                    <input type="text" name="content" placeholder="Type here to add comment..." required>
+                    <input type="hidden" name="post" value="${post._id}">
+                    <input type="submit" value="Add Comment">
+                </form>
+    
+        </div>
+        <div class="post-comments-list">
+            <ul id="post-comments-${post._id}">
+            
+            </ul>
+        </div>
+      </li>`)
+    }
+
+    createPost();
 }
