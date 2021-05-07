8d8f984e8e3854fa0054a3ade40477ee91a87120
Rohan Gupta <rohangupta569@gmail.com>
2021-05-07 23:23:02 +0530

    delete post using api without authentication

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,4 +1,5 @@
 const Post = require('../../../models/post');
+const Comment = require('../../../models/comment');
 
 module.exports.index = async function(req,res){
     let posts = await Post.find({})
@@ -14,4 +15,32 @@ module.exports.index = async function(req,res){
         message: 'List of Posts',
         posts: posts
     });
+}
+
+module.exports.destroy = async function(req,res){
+    try {
+        let post = await Post.findById(req.params.id);
+        //console.log(post);
+
+
+     //.id means converting the object id into string
+     //if(post.user == req.user.id){
+       post.remove();
+
+        await Comment.deleteMany({post:req.params.id});
+
+        return res.json(200,{
+            message: 'Post and associated comments deleted successfully!'
+        });
+    //  }else{
+    //     req.flash('error','You cannot delete this post!');
+    //     return res.redirect('back');
+    //  }   
+    } catch (err) {
+        console.log('******' , err);
+        return res.json(500,{
+            message : 'Internal Server Error'
+        });
+    }
+        
 }
