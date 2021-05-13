9e6844bbb40e92a1cddf4d557e17409fb296e933
Rohan Gupta <rohangupta569@gmail.com>
2021-05-14 02:33:28 +0530

    Authorization and authentication using jwt token

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -24,7 +24,7 @@ module.exports.destroy = async function(req,res){
 
 
      //.id means converting the object id into string
-     //if(post.user == req.user.id){
+     if(post.user == req.user.id){
        post.remove();
 
         await Comment.deleteMany({post:req.params.id});
@@ -32,10 +32,11 @@ module.exports.destroy = async function(req,res){
         return res.json(200,{
             message: 'Post and associated comments deleted successfully!'
         });
-    //  }else{
-    //     req.flash('error','You cannot delete this post!');
-    //     return res.redirect('back');
-    //  }   
+     }else{
+        return res.json(401,{
+            message:'You cannot delete this post!'
+        });
+     }   
     } catch (err) {
         console.log('******' , err);
         return res.json(500,{
