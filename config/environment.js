c5b8d62e1409041782dac24b2bcccfa28e574897
Rohan Gupta <rohangupta569@gmail.com>
2022-02-11 03:18:49 +0530

    Environment Variables : Production

--- config/environment.js
+++ config/environment.js
@@ -21,7 +21,24 @@ const development = {
 }
 
 const production = {
-    name : 'production'
+    name : 'production',
+    asset_path : process.env.codeial_asset_path,
+    session_cookie_key : process.env.CODEIAL_SESSION_COOKIE_KEY,
+    db: process.env.CODEIAL_DB,
+    smtp : {
+        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
+        auth: {
+            user:process.env.CODEIAL_GMAIL_USERNAME,
+            pass: process.env.CODEIAL_GMAIL_PASSWORD
+        }
+    },
+    google_client_id: process.env.CODEIAL_GOOGLE_CLIENT_ID,
+    google_client_secret: process.env.CODEIAL_GOOGLE_CLIENT_SECRET,
+    google_call_back_url: process.env.GOOGLE_CALL_BACK_URL,
+    jwt_secret : process.env.CODEIAL_JWT_SECRET
 }
 
-module.exports = development;
+module.exports = eval(process.env.CODEIAL_ENVIRONMENT) == undefined ? development : eval(process.env.CODEIAL_ENVIRONMENT);
