a4cb1fa1e4de0da4e795d13241a44f8528d57998
Rohan Gupta <rohangupta569@gmail.com>
2022-07-07 23:44:00 +0530

    Adding mongo atlas

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 const env = require('./environment');
 
-mongoose.connect(`mongodb://localhost/${env.db}`, {useNewUrlParser: true, useUnifiedTopology: true});
+//mongoose.connect(`mongodb://localhost/${env.db}`, {useNewUrlParser: true, useUnifiedTopology: true});
+const uri = "mongodb+srv://rohangupta569:mongodb@1234@cluster0.4jubb.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(uri,{useNewUrlParser: true, useUnifiedTopology: true});
 
 const db = mongoose.connection;
 
